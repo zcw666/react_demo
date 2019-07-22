166854303854c7b4a85211ce70bab9c1da658227
weichen <webgit@163.com>
2019-07-22 11:53:44 +0800

    '定义当前项目代码风格'

--- note.md
+++ note.md
@@ -10,7 +10,9 @@
 
 ### 格式化缩进 4 格
 
-配置明明是 4 格，总是莫名的变两格
+1、在项目根创建一个名为 .editorconfig 的文件。该文件的内容定义该项目的编码规范。EditorConfig 支持的编码规范在后文会有详细的介绍。
+2、安装与编辑器对应的 EditorConfig 插件。
+https://www.jianshu.com/p/712cea0ef70e
 
 ### 消除每个方法都要绑定 this
 
--- src/login/index.js
+++ src/login/index.js
@@ -3,120 +3,123 @@ import $axios from "axios";
 import "./index.css";
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.register = this.register.bind(this);
-    this.state = {
-      text: "注册",
-      getCodeText: "获取验证码",
-      phoneNumber: "",
-      code: ""
-    };
-  }
-  render() {
-    return (
-      <div className="login">
-        <div className="content">
-          <div className="title">知乎</div>
-          <div className="text">
-            {this.state.text}知乎，发现更多可信赖的解答
-          </div>
-          <div className="phone">
-            <div className="phone-title">手机号：</div>
-            <input
-              className="phone-num"
-              value={this.state.phoneNumber}
-              onChange={this.getPhone.bind(this)}
-            />
-          </div>
-          <div className="code">
-            <input
-              className="phone-code"
-              value={this.state.code}
-              onChange={this.chongeCode.bind(this)}
-            />
-            <div className="get-code" onClick={this.getCode.bind(this)}>
-              {this.state.getCodeText}
+    constructor(props) {
+        super(props);
+        this.register = this.register.bind(this);
+        this.state = {
+            text: "注册",
+            getCodeText: "获取验证码",
+            phoneNumber: "",
+            code: ""
+        };
+    }
+    render() {
+        return (
+            <div className="login">
+                <div className="content">
+                    <div className="title">知乎</div>
+                    <div className="text">
+                        {this.state.text}知乎，发现更多可信赖的解答
+                    </div>
+                    <div className="phone">
+                        <div className="phone-title">手机号：</div>
+                        <input
+                            className="phone-num"
+                            value={this.state.phoneNumber}
+                            onChange={this.getPhone.bind(this)}
+                        />
+                    </div>
+                    <div className="code">
+                        <input
+                            className="phone-code"
+                            value={this.state.code}
+                            onChange={this.chongeCode.bind(this)}
+                        />
+                        <div
+                            className="get-code"
+                            onClick={this.getCode.bind(this)}
+                        >
+                            {this.state.getCodeText}
+                        </div>
+                    </div>
+                    <div className="register-button" onClick={this.register}>
+                        注册
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="register-button" onClick={this.register}>
-            注册
-          </div>
-        </div>
-      </div>
-    );
-  }
-  componentWillMount() {
-    console.log("");
-  }
-  componentDidMount() {
-    console.log("");
-  }
-  shouldComponentUpdate() {
-    console.log("");
-  }
-  componentWillUpdate() {
-    console.log("");
-  }
-  componentDidUpdate() {
-    console.log("");
-  }
-  componentWillReceiveProps() {
-    console.log("componentWillReceiveProps");
-  }
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-  }
-  register() {
-    const URL =
-      "https://www.easy-mock.com/mock/5d345a29e332a27a496bbe7e/zhihu/regisrer?phoneNumber=" +
-      this.state.phoneNumber +
-      "code=" +
-      this.state.code;
+        );
+    }
+    componentWillMount() {
+        console.log("componentWillMount");
+    }
+    componentDidMount() {
+        console.log("componentDidMount");
+    }
+    shouldComponentUpdate() {
+        console.log("shouldComponentUpdate");
+    }
+    componentWillUpdate() {
+        console.log("componentWillUpdate");
+    }
+    componentDidUpdate() {
+        console.log("componentDidUpdate");
+    }
+    componentWillReceiveProps() {
+        console.log("componentWillReceiveProps");
+    }
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+    register() {
+        const URL =
+            "https://www.easy-mock.com/mock/5d345a29e332a27a496bbe7e/zhihu/regisrer?phoneNumber=" +
+            this.state.phoneNumber +
+            "code=" +
+            this.state.code;
 
-    $axios
-      .post(URL)
-      .then(res => {
-        console.log("then", res);
-        const data = res.data;
-        if (data.code === 1) {
-          alert(data.msg);
-        }
-      })
-      .catch(err => {
-        console.log("catch", err);
-      });
-  }
-  getPhone(e) {
-    this.setState({
-      phoneNumber: e.target.value
-    });
-  }
-  chongeCode(e) {
-    this.setState({
-      code: e.target.value
-    });
-  }
-  getCode() {
-    this.setState({
-      getCodeText: "60秒后可重发"
-    });
-    let num = 60;
-    const set = setInterval(() => {
-      console.log("获取验证码1");
-      num--;
-
-      this.setState({
-        getCodeText: num + "秒后可重发"
-      });
-      if (num <= 0) {
-        clearInterval(set);
+        $axios
+            .post(URL)
+            .then(res => {
+                console.log("then", res);
+                const data = res.data;
+                if (data.code === 1) {
+                    alert(data.msg);
+                }
+            })
+            .catch(err => {
+                console.log("catch", err);
+            });
+    }
+    getPhone(e) {
         this.setState({
-          getCodeText: "重新获取验证码"
+            phoneNumber: e.target.value
         });
-      }
-    }, 1000);
-  }
+    }
+    chongeCode(e) {
+        this.setState({
+            code: e.target.value
+        });
+    }
+    getCode() {
+        this.setState({
+            getCodeText: "60秒后可重发"
+        });
+        let num = 60;
+        const set = setInterval(() => {
+            console.log("获取验证码1");
+            num--;
+
+            this.setState({
+                getCodeText: num + "秒后可重发"
+            });
+            if (num <= 0) {
+                clearInterval(set);
+                this.setState({
+                    getCodeText: "重新获取验证码"
+                });
+            }
+        }, 1000);
+    }
 }
 
 export default Login;
